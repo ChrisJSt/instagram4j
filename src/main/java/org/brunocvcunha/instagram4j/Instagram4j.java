ef393bcfdcc390056ee4f0b31e2966519d505eeb
ChrisJSt <chrisjstelmach@gmail.com>
2017-12-12 13:48:26 -0500

    Adding forked comment to main file

--- src/main/java/org/brunocvcunha/instagram4j/Instagram4j.java
+++ src/main/java/org/brunocvcunha/instagram4j/Instagram4j.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+//THIS IS THE FORKED REPO of CHRIJST
 package org.brunocvcunha.instagram4j;
 
 import java.io.IOException;
